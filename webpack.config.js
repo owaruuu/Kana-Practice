64730bb2f2fbe497aacaac2b37bb2037bec555b8
Josue Marquez <owaruuu@gmail.com>
2024-02-26 21:59:33 -0300

    webpack update
    -move css to root in assets folder
    -move where index searches for css
    -move index.html to root
    -modify dev-server script call
    -install copy-webpack-plugin to copy the assets to dist
    -modify webpack config to serve files from root

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 const webpack = require("webpack");
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const config = {
-    mode: "none",
+    mode: "development",
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
     },
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, ""),
+        },
+        hot: true,
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "assets", to: "assets" }, //to the dist root directory
+            ],
+        }),
+    ],
 };
 
 module.exports = config;
