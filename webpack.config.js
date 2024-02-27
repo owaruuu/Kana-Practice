5635b3feae32dbd286e76e88c14dd4ed8ad2b752
Josue Marquez <owaruuu@gmail.com>
2024-02-26 22:04:12 -0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const config = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, ""),
+        },
+        hot: true,
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "assets", to: "assets" }, //to the dist root directory
+            ],
+        }),
+    ],
+};
+
+module.exports = config;
