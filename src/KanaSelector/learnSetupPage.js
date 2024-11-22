a7c33a1a1a2c9420eb8af9db57a64f1912150261
Josue Marquez <owaruuu@gmail.com>
2024-11-22 17:13:15 -0300

    Update learnSetupPage.js
    clean old code

--- src/KanaSelector/learnSetupPage.js
+++ src/KanaSelector/learnSetupPage.js
@@ -1,19 +1,13 @@
 import {
     CleanAppPage,
     PopulateInstructions,
-    CreateStackedButton,
     ChangeScreen,
 } from "../helpers/helpers.js";
 import { CreateSetupButtons } from "./kanaSelector.js";
 import { instrucciones } from "../data/data.js";
 import { GetLearnSets, setState, state } from "../state/state.js";
 
-import {
-    CreateSimple,
-    CreateComplex,
-    CreateAndClass,
-    CreateAndId,
-} from "../helpers/domHelpers.js";
+import { CreateComplex, CreateAndClass } from "../helpers/domHelpers.js";
 
 let startButton = null;
 
@@ -69,55 +63,3 @@ function CheckSelected() {
 
     ChangeScreen(startButton.getAttribute("type"));
 }
-
-// function BuildLearnSetupPage() {
-//     let app = CleanAppPage();
-
-//     PopulateInstructions(instrucciones.learn);
-
-//     let practiceSetupDiv = CreateAndClass("div", app, ["setupDiv"]);
-
-//     CreateSetupButtons(practiceSetupDiv);
-
-//     startButton = CreateComplex(
-//         "button",
-//         app,
-//         null,
-//         ["start-button"],
-//         "Aprender"
-//     );
-
-//     startButton.addEventListener("click", CheckLearnSelected);
-// }
-
-// /**
-//  * Checks which config buttons are pressed and then builds the Learn Page
-//  *
-//  */
-// function CheckLearnSelected() {
-//     //remove listener
-//     startButton.removeEventListener("click", CheckLearnSelected);
-
-//     //get all labels
-//     let buttons = document.querySelectorAll("div.checkboxes > div > label");
-
-//     //hacer un array con todos los 'check'
-//     let checked = [];
-
-//     buttons.forEach((button) => {
-//         if (button.classList.contains("check")) {
-//             checked.push(button);
-//         }
-//     });
-
-//     if (checked.length < 1) {
-//         startButton.addEventListener("click", CheckLearnSelected);
-//         alert("Por favor selecciona lo que quieres practicar.");
-//         return;
-//     }
-
-//     const sets = GetLearnSets(checked);
-//     setState({ ...state, learnSets: sets, currentSet: sets[0] });
-
-//     ChangeScreen("learn");
-// }
